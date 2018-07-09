444d914d24d0cba1e4f95c77ef8bf8227c1ed6ab
Marius Guggenmos <MariusGuggenmos@gmail.com>
2018-07-09 20:35:04 +0200

    fixed compile 2 cmovs sample commands

--- compile_samples.sh
+++ compile_samples.sh
@@ -14,6 +14,6 @@ $ollvm quarkslab.c -o quarkslab_org.elf
 $ollvm -mllvm -fla hash.c -o hash.elf
 $ollvm hash.c -o hash_org.elf
 
-$ollvm -mllvm -fla fla_test.c -o fla_test_2_cmovs.elf
-$ollvm -mllvm -fla fla_test.c -o fla_test_2_cmovs_opt.elf -O2
-$ollvm fla_test.c -o fla_test_2_cmovs_org.elf
+$ollvm -mllvm -fla fla_test_2_cmovs.c -o fla_test_2_cmovs.elf
+$ollvm -mllvm -fla fla_test_2_cmovs.c -o fla_test_2_cmovs_opt.elf -O2
+$ollvm fla_test_2_cmovs.c -o fla_test_2_cmovs_org.elf
